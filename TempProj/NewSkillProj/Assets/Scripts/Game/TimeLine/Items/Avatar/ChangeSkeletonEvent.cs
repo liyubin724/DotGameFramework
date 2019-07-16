13c26edc8a82f0f702c71139f2ccbf00ae1dd080
yubin.li <yubin.li@leyoutech.com.cn>
2019-07-16 10:38:57 +0800

    添加骨骼控制

---
+++ TempProj/NewSkillProj/Assets/Scripts/Game/TimeLine/Items/Avatar/ChangeSkeletonEvent.cs
@@ -0,0 +1,19 @@
+﻿using Dot.Core.TimeLine.Base;
+using Dot.Core.TimeLine.Base.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.TimeLine
+{
+    //[TimeLineMark("Event/Avatar", "Sel", TimeLineExportPlatform.Client)]
+    public class ChangeSkeletonEvent : AEventItem
+    {
+        public override void Trigger()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
