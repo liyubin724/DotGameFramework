02cab2acc6d93e59bcb6a06d4ded8aea3cd95a92
yubin.li <yubin.li@leyoutech.com.cn>
2019-07-22 11:19:38 +0800

    编辑新技能及子弹效果，修改移动的问题

--- TempProj/NewSkillProj/Assets/Scripts/Game/Motion/MoverSystem.cs
+++ TempProj/NewSkillProj/Assets/Scripts/Game/Motion/MoverSystem.cs
@@ -23,49 +23,50 @@ public class MoverSystem : IExecuteSystem
         {
             if(e.isMover)
             {
-                if(e.hasMotionCurveType)
+                if (e.hasEntityTarget || e.hasPositionTarget)
                 {
-                    if(e.motionCurveType.value == MotionCurveType.Linear)
+                    Vector3 targetPos = Vector3.zero;
+                    if (e.hasEntityTarget)
                     {
-                        if (e.hasEntityTarget || e.hasPositionTarget)
-                        {
-                            Vector3 targetPos = Vector3.zero;
-                            if(e.hasEntityTarget)
-                            {
-                                targetPos = contexts.game.GetEntityWithUniqueID(e.entityTarget.entityID).position.value;
-                            }else
-                            {
-                                targetPos = e.positionTarget.value;
-                            }
-                            Vector3 direction = (targetPos - e.position.value).normalized;
-                            if(Vector3.Dot(e.direction.value,direction)<0)
-                            {
-                                e.isMover = false;
-                                continue;
-                            }
-                            if((targetPos - e.position.value).magnitude <= 0.01f)
-                            {
-                                e.isMover = false;
-                                continue;
-                            }
+                        targetPos = contexts.game.GetEntityWithUniqueID(e.entityTarget.entityID).position.value;
+                    }
+                    else
+                    {
+                        targetPos = e.positionTarget.value;
+                    }
+                    Vector3 direction = (targetPos - e.position.value).normalized;
+                    if (Vector3.Dot(e.direction.value, direction) < 0)
+                    {
+                        e.isMover = false;
+                        continue;
+                    }
+                    if ((targetPos - e.position.value).magnitude <= 0.01f)
+                    {
+                        e.isMover = false;
+                        continue;
+                    }
 
-                            if (e.hasEntityTarget)
-                            {
-                                GameEntity targetEntity = contexts.game.GetEntityWithUniqueID(e.entityTarget.entityID);
-                                e.ReplaceDirection((targetEntity.position.value - e.position.value).normalized);
-                            }
-                        }
-
+                    if (e.hasEntityTarget)
+                    {
+                        GameEntity targetEntity = contexts.game.GetEntityWithUniqueID(e.entityTarget.entityID);
+                        e.ReplaceDirection((targetEntity.position.value - e.position.value).normalized);
+                    }
+                }
+                
+                if (e.hasMotionCurveType)
+                {
+                    if (e.motionCurveType.value == MotionCurveType.Linear)
+                    {
                         float curSpeed = e.hasSpeed ? e.speed.value : 0.0f;
                         float curAcc = e.hasAcceleration ? e.acceleration.value : 0.0f;
 
                         float speed = curSpeed + curAcc * services.timeService.DeltaTime();
-                        if(e.hasMaxSpeed)
+                        if (e.hasMaxSpeed)
                         {
                             speed = Mathf.Min(speed, e.maxSpeed.value);
                         }
                         Vector3 deltaPosition = (e.direction.value * speed + (e.direction.value * curAcc) * services.timeService.DeltaTime()) * services.timeService.DeltaTime();
-                        
+
                         e.ReplaceSpeed(speed);
                         e.ReplacePosition(e.position.value + deltaPosition);
                     }
