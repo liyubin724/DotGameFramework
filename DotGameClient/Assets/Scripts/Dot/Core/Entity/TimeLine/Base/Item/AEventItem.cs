d855370990ed65508e1eea43aac78357878619a4
yubin.li <yubin.li@leyoutech.com.cn>
2019-07-30 14:38:58 +0800

    添加Timeline，可用于添加基于时间的事件触发

---
+++ DotGameClient/Assets/Scripts/Dot/Core/Entity/TimeLine/Base/Item/AEventItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Dot.Core.TimeLine
+{
+    public abstract class AEventItem : AItem
+    {
+        public bool CanRevert { get; set; } = true;
+
+        public virtual void DoRevert()
+        {
+        }
+        public abstract void Trigger();
+    }
+}
