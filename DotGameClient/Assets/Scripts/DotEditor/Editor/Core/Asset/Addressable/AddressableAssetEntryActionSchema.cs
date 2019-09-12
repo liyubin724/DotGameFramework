fc7f9a1b2fe30a79850037f361258cf4f055af34
yubin.li <yubin.li@leyoutech.com.cn>
2019-09-12 18:06:35 +0800

    禁用Addressable配置工具

--- DotGameClient/Assets/Scripts/DotEditor/Editor/Core/Asset/Addressable/AddressableAssetEntryActionSchema.cs
+++ DotGameClient/Assets/Scripts/DotEditor/Editor/Core/Asset/Addressable/AddressableAssetEntryActionSchema.cs
@@ -1,87 +1,87 @@
-﻿using Dot.Core.Util;
-using Sirenix.OdinInspector;
-using System.IO;
-using UnityEditor;
-//using UnityEditor.AddressableAssets;
-//using UnityEditor.AddressableAssets.Settings;
-using UnityEngine;
+﻿//using Dot.Core.Util;
+//using Sirenix.OdinInspector;
+//using System.IO;
+//using UnityEditor;
+////using UnityEditor.AddressableAssets;
+////using UnityEditor.AddressableAssets.Settings;
+//using UnityEngine;
 
-namespace DotEditor.Core.Asset
-{
+//namespace DotEditor.Core.Asset
+//{
     
 
-    [CreateAssetMenu(fileName = "addressable_entry_scheme", menuName = "Asset/Action/Addressable/Entry Action Scheme")]
-    public class AddressableAssetEntryActionSchema : BaseActionSchema
-    {
-        public AssetAddressMode addressType = AssetAddressMode.FullPath;
-        [ShowIf("IsShowFileNameFormat")]
-        public string fileNameFormat = "{0}";
-        public string filterFolder = "";
-        public string[] labels = new string[0];
+//    [CreateAssetMenu(fileName = "addressable_entry_scheme", menuName = "Asset/Action/Addressable/Entry Action Scheme")]
+//    public class AddressableAssetEntryActionSchema : BaseActionSchema
+//    {
+//        public AssetAddressMode addressType = AssetAddressMode.FullPath;
+//        [ShowIf("IsShowFileNameFormat")]
+//        public string fileNameFormat = "{0}";
+//        public string filterFolder = "";
+//        public string[] labels = new string[0];
 
-        public void Execute(AssetGroupActionData actionData)
-        {
-            if (!isEnable) return;
+//        public void Execute(AssetGroupActionData actionData)
+//        {
+//            if (!isEnable) return;
 
-            //AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
-            //AddressableAssetGroup assetGroup = settings.FindGroup(actionData.groupName);
-            //if(assetGroup == null)
-            //{
-            //    Debug.LogError("AddressableAssetEntryActionSchema::Execute->Group Not Found");
-            //    return;
-            //}
-            //if (actionData.filterDatas != null && actionData.filterDatas.Count > 0)
-            //{
+//            //AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+//            //AddressableAssetGroup assetGroup = settings.FindGroup(actionData.groupName);
+//            //if(assetGroup == null)
+//            //{
+//            //    Debug.LogError("AddressableAssetEntryActionSchema::Execute->Group Not Found");
+//            //    return;
+//            //}
+//            //if (actionData.filterDatas != null && actionData.filterDatas.Count > 0)
+//            //{
                 
-            //    foreach (var filterData in actionData.filterDatas)
-            //    {
-            //        if (!string.IsNullOrEmpty(filterFolder) && filterFolder != filterData.filterFolder)
-            //        {
-            //            continue;
-            //        }
-            //        foreach (var asset in filterData.assets)
-            //        {
-            //            if (asset != null)
-            //            {
-            //                string guid = AssetDatabase.AssetPathToGUID(asset);
-            //                AddressableAssetEntry assetEntry = assetGroup.GetAssetEntry(guid);
-            //                if (assetEntry == null)
-            //                {
-            //                    assetEntry = settings.CreateOrMoveEntry(guid, assetGroup,false,false);
-            //                }
-            //                assetEntry.SetAddress(GetAssetAddress(asset),false);
-            //                if(labels!=null && labels.Length>0)
-            //                {
-            //                    foreach(var label in labels)
-            //                    {
-            //                        assetEntry.SetLabel(label, true,true,false);
-            //                    }
-            //                }
-            //            }
-            //        }
-            //    }
+//            //    foreach (var filterData in actionData.filterDatas)
+//            //    {
+//            //        if (!string.IsNullOrEmpty(filterFolder) && filterFolder != filterData.filterFolder)
+//            //        {
+//            //            continue;
+//            //        }
+//            //        foreach (var asset in filterData.assets)
+//            //        {
+//            //            if (asset != null)
+//            //            {
+//            //                string guid = AssetDatabase.AssetPathToGUID(asset);
+//            //                AddressableAssetEntry assetEntry = assetGroup.GetAssetEntry(guid);
+//            //                if (assetEntry == null)
+//            //                {
+//            //                    assetEntry = settings.CreateOrMoveEntry(guid, assetGroup,false,false);
+//            //                }
+//            //                assetEntry.SetAddress(GetAssetAddress(asset),false);
+//            //                if(labels!=null && labels.Length>0)
+//            //                {
+//            //                    foreach(var label in labels)
+//            //                    {
+//            //                        assetEntry.SetLabel(label, true,true,false);
+//            //                    }
+//            //                }
+//            //            }
+//            //        }
+//            //    }
 
                 
-            //}
-        }
+//            //}
+//        }
 
-        private string GetAssetAddress(string assetPath)
-        {
-            if (addressType == AssetAddressMode.FullPath)
-                return assetPath;
-            else if (addressType == AssetAddressMode.FileName)
-                return Path.GetFileName(assetPath);
-            else if (addressType == AssetAddressMode.FileNameWithoutExtension)
-                return Path.GetFileNameWithoutExtension(assetPath);
-            else if (addressType == AssetAddressMode.FileFormatName)
-                return string.Format(fileNameFormat, Path.GetFileNameWithoutExtension(assetPath));
-            else
-                return assetPath;
-        }
+//        private string GetAssetAddress(string assetPath)
+//        {
+//            if (addressType == AssetAddressMode.FullPath)
+//                return assetPath;
+//            else if (addressType == AssetAddressMode.FileName)
+//                return Path.GetFileName(assetPath);
+//            else if (addressType == AssetAddressMode.FileNameWithoutExtension)
+//                return Path.GetFileNameWithoutExtension(assetPath);
+//            else if (addressType == AssetAddressMode.FileFormatName)
+//                return string.Format(fileNameFormat, Path.GetFileNameWithoutExtension(assetPath));
+//            else
+//                return assetPath;
+//        }
 
-        #region odin
-        private bool IsShowFileNameFormat() => addressType == AssetAddressMode.FileFormatName;
+//        #region odin
+//        private bool IsShowFileNameFormat() => addressType == AssetAddressMode.FileFormatName;
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
--- DotGameClient/Assets/Scripts/DotEditor/Editor/Core/Asset/Addressable/AddressableSchemaSetting.cs
+++ DotGameClient/Assets/Scripts/DotEditor/Editor/Core/Asset/Addressable/AddressableSchemaSetting.cs
@@ -1,49 +1,49 @@
-﻿using DotEditor.Core.Util;
-using Sirenix.OdinInspector;
-using System;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿//using DotEditor.Core.Util;
+//using Sirenix.OdinInspector;
+//using System;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
 
-namespace DotEditor.Core.Asset
-{
-    [CreateAssetMenu(fileName = "addressable_asset_setting", menuName = "Asset/Asset Setting/Addressable")]
-    public class AddressableSchemaSetting : BaseAssetSchemaSetting
-    {
-        [PropertyOrder(300)]
-        [Button("Execute Addressable", ButtonSizes.Large)]
-        public void ExecuteAddressable()
-        {
-            if (groupSchemas != null && groupSchemas.Length > 0)
-            {
-                Array.ForEach(groupSchemas, (group) =>
-                {
-                   // group.Execute();
-                });
-            }
+//namespace DotEditor.Core.Asset
+//{
+//    [CreateAssetMenu(fileName = "addressable_asset_setting", menuName = "Asset/Asset Setting/Addressable")]
+//    public class AddressableSchemaSetting : BaseAssetSchemaSetting
+//    {
+//        [PropertyOrder(300)]
+//        [Button("Execute Addressable", ButtonSizes.Large)]
+//        public void ExecuteAddressable()
+//        {
+//            if (groupSchemas != null && groupSchemas.Length > 0)
+//            {
+//                Array.ForEach(groupSchemas, (group) =>
+//                {
+//                   // group.Execute();
+//                });
+//            }
 
-            AssetDatabase.SaveAssets();
+//            AssetDatabase.SaveAssets();
 
-            EditorUtility.DisplayDialog("Complete", "Set Asset Complete", "OK");
-        }
-        [PropertyOrder(300)]
-        [Button("Clean Addressable", ButtonSizes.Large)]
-        public void CleanAddressable()
-        {
-            if (groupSchemas != null && groupSchemas.Length > 0)
-            {
-                Array.ForEach(groupSchemas, (group) =>
-                {
-                    //AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
-                    //AddressableAssetGroup assetGroup = settings.FindGroup(group.groupName);
-                    //if(assetGroup!=null)
-                    //{
-                    //    settings.RemoveGroup(assetGroup);
-                    //}
-                });
-            }
+//            EditorUtility.DisplayDialog("Complete", "Set Asset Complete", "OK");
+//        }
+//        [PropertyOrder(300)]
+//        [Button("Clean Addressable", ButtonSizes.Large)]
+//        public void CleanAddressable()
+//        {
+//            if (groupSchemas != null && groupSchemas.Length > 0)
+//            {
+//                Array.ForEach(groupSchemas, (group) =>
+//                {
+//                    //AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+//                    //AddressableAssetGroup assetGroup = settings.FindGroup(group.groupName);
+//                    //if(assetGroup!=null)
+//                    //{
+//                    //    settings.RemoveGroup(assetGroup);
+//                    //}
+//                });
+//            }
 
-            EditorUtility.DisplayDialog("Complete", "Clean Asset Complete", "OK");
-        }
-    }
-}
+//            EditorUtility.DisplayDialog("Complete", "Clean Asset Complete", "OK");
+//        }
+//    }
+//}
